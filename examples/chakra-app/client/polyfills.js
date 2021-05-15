b865bd5ae04b5ddc9ddd7e8652e3f6d406fe0d6e
Justin Hanselman <hanseltime@gmail.com>
2021-05-15 15:13:42 -0500

    chore: updating example app to work

---
+++ examples/chakra-app/client/polyfills.js
@@ -0,0 +1,15 @@
+/**
+ * Object.entriesFrom() polyfill
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+ if (!Object.fromEntries) {
+	Object.fromEntries = function (entries){
+		if (!entries || !entries[Symbol.iterator]) { throw new Error('Object.fromEntries() requires a single iterable argument'); }
+		let obj = {};
+		for (let [key, value] of entries) {
+			obj[key] = value;
+		}
+		return obj;
+	};
+}
---
+++ examples/chakra-app/next.config.js
@@ -0,0 +1,78 @@
+const withPlugins = require('next-compose-plugins');
+const withTranspileModules = require('next-transpile-modules')([
+    '@chakra-ui/react',
+    '@chakra-ui/color-mode',
+    '@chakra-ui/accordion',
+'@chakra-ui/alert',
+'@chakra-ui/avatar',
+'@chakra-ui/breadcrumb',
+'@chakra-ui/button',
+'@chakra-ui/checkbox',
+'@chakra-ui/clickable',
+'@chakra-ui/close-button',
+'@chakra-ui/color-mode',
+'@chakra-ui/control-box',
+'@chakra-ui/counter',
+'@chakra-ui/css-reset',
+'@chakra-ui/descendant',
+'@chakra-ui/editable',
+'@chakra-ui/focus-lock',
+'@chakra-ui/form-control',
+'@chakra-ui/hooks',
+'@chakra-ui/icon',
+'@chakra-ui/image',
+'@chakra-ui/input',
+'@chakra-ui/layout',
+'@chakra-ui/live-region',
+'@chakra-ui/media-query',
+'@chakra-ui/menu',
+'@chakra-ui/modal',
+'@chakra-ui/number-input',
+'@chakra-ui/pin-input',
+'@chakra-ui/popover',
+'@chakra-ui/popper',
+'@chakra-ui/portal',
+'@chakra-ui/progress',
+'@chakra-ui/radio',
+'@chakra-ui/react',
+'@chakra-ui/react-env',
+'@chakra-ui/react-utils',
+'@chakra-ui/select',
+'@chakra-ui/skeleton',
+'@chakra-ui/slider',
+'@chakra-ui/spinner',
+'@chakra-ui/stat',
+'@chakra-ui/styled-system',
+'@chakra-ui/switch',
+'@chakra-ui/system',
+'@chakra-ui/table',
+'@chakra-ui/tabs',
+'@chakra-ui/tag',
+'@chakra-ui/textarea',
+'@chakra-ui/theme',
+'@chakra-ui/theme-tools',
+'@chakra-ui/toast',
+'@chakra-ui/tooltip',
+'@chakra-ui/transition',
+'@chakra-ui/utils',
+'@chakra-ui/visually-hidden',
+'rgba-hex-to-func',
+'css-vars-revert'
+]);
+
+const nextConfig = {
+    webpack: (config, options) => {
+        console.log("In this method Fam");
+        const originalEntry = config.entry;
+        config.entry = async () => {
+            const entries = await originalEntry();
+            if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
+                entries['main.js'].unshift('./client/polyfills.js');
+            }
+            return entries;
+        }
+        return config;
+    }
+}
+
+module.exports = withPlugins([withTranspileModules], nextConfig);
